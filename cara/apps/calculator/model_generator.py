7cd4e4ac8521aae85832ada845d7ef44a1c816e4
markus <marro98@gmail.com>
2020-11-06 12:13:01 +0100

    de-capitalize activity_dict

--- cara/apps/calculator/model_generator.py
+++ cara/apps/calculator/model_generator.py
@@ -188,9 +188,9 @@ def model_from_form(form: FormData, tmp_raw_form_data) -> models.Model:
     # of the infected and exposed occupants respectively.
     # I.e. (infected_activity, infected_expiration), (exposed_activity, exposed_expiration)
 
-    activity_dict = {'Office/Meeting': (('Seated', 'Talking'), ('Seated', 'Talking')),
-                     'Training': (('Light exercise', 'Talking'), ('Seated', 'Whispering')),
-                     'Workshop': (('Light exercise', 'Talking'), ('Light exercise', 'Talking'))}
+    activity_dict = {'office': (('Seated', 'Talking'), ('Seated', 'Talking')),
+                     'training': (('Light exercise', 'Talking'), ('Seated', 'Whispering')),
+                     'workshop': (('Light exercise', 'Talking'), ('Light exercise', 'Talking'))}
 
     (infected_activity, infected_expiration), (exposed_activity, exposed_expiration) = activity_dict[form.activity_type]
     # Converts these strings to Activity and Expiration instances
