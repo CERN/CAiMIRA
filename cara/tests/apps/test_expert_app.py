936b339edfdadf86532398d3bd5df3bdbf99b218
markus <marro98@gmail.com>
2020-11-17 13:36:28 +0100

    fix pipeline

--- cara/tests/apps/test_expert_app.py
+++ cara/tests/apps/test_expert_app.py
@@ -6,4 +6,4 @@ def test_app():
     # do anything fancy to verify how it looks etc., we leave that for manual
     # testing.
     expert_app = cara.apps.ExpertApplication()
-    assert expert_app.scenario_names[0] == "Scenario 1"
+    assert expert_app.multi_model_view.scenario_names[0] == "Scenario 1"
