ce453fc269033ffe194ca40dade4a62665b983fb
markus <marro98@gmail.com>
2021-01-18 15:15:13 +0100

    rename emission_concentration to emission

--- cara/montecarlo.py
+++ cara/montecarlo.py
@@ -11,10 +11,11 @@ weibull_parameters = {
     'particle_diameter_speaking_loudly': (7.348365409721486, 1.1158159287760463)
 }
 
-def calculate_qr(viral_load: float, emission_concentration: float, diameter: float, mask_efficiency: float,
+
+def calculate_qr(viral_load: float, emission: float, diameter: float, mask_efficiency: float,
                  copies_per_quantum: float, breathing_rate: float = 1) -> float:
     """
     Calculates the quantum generation rate given a set of parameters.
     """
     volume = 4 * np.pi * (diameter / 2) ** 3 / 3
-    return viral_load * emission_concentration * volume * (1 - mask_efficiency) * breathing_rate / copies_per_quantum
+    return viral_load * emission * volume * (1 - mask_efficiency) * breathing_rate / copies_per_quantum
