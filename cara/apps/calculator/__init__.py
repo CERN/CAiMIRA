05dffa3f50409d1c82b064541c008e42fb5dd1a4
Phil Elson <philip.elson@cern.ch>
2021-05-03 13:46:03 +0200

    Compromise on the header information added to key files, ensuring that the details are correct, but are canonically defined in one place.

--- cara/apps/calculator/__init__.py
+++ cara/apps/calculator/__init__.py
@@ -1,3 +1,6 @@
+# This module is part of CARA. Please see the repository at
+# https://gitlab.cern.ch/cara/cara for details of the license and terms of use.
+
 import datetime
 import base64
 import html
--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+# This module is part of CARA. Please see the repository at
+# https://gitlab.cern.ch/cara/cara for details of the license and terms of use.
 """
 setup.py for CARA.
 
