22539e2f9fbb3e77cbbb7a845de90b4030dabff8
Nicolas Mounet <nicolas.mounet@cern.ch>
2021-05-27 18:17:59 +0200

    Adding test on BLO expiration

--- cara/tests/test_expiration.py
+++ cara/tests/test_expiration.py
@@ -23,7 +23,15 @@ def test_multiple():
     e1 = models.Expiration((0.03, 0.02, 0.01, 0.005))
     e2 = models.Expiration((0.05, 0.04, 0.03, 0.01))
     e = models.MultipleExpiration([e1, e2], weights)
-    assert e.aerosols(models.Mask.types['No mask']) == (
-              e1.aerosols(models.Mask.types['No mask'])/3. +
-            2*e2.aerosols(models.Mask.types['No mask'])/3.
-    )
+    mask = models.Mask.types['No mask']
+    assert e.aerosols(mask) == e1.aerosols(mask)/3. + 2*e2.aerosols(mask)/3.
+
+
+def test_multiple_BLO():
+    weights = (1., 1.)
+    e1 = models.ExpirationBLO((1., 0., 0.))
+    e2 = models.ExpirationBLO((4., 5., 5.))
+    e_expected = models.ExpirationBLO((2.5, 2.5, 2.5))
+    e = models.MultipleExpiration([e1, e2], weights)
+    mask = models.Mask.types['Type I']
+    assert e_expected.aerosols(mask) == e.aerosols(mask)
