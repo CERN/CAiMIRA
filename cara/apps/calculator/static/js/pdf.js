d8e539c9d330c06d9258515e7eafc363d39a1c55
Luis Aleixo <luis.aleixo@cern.ch>
2021-08-23 11:22:15 +0200

    added responsive size for alternative scenarios

--- cara/apps/calculator/static/js/pdf.js
+++ cara/apps/calculator/static/js/pdf.js
@@ -1,11 +1,6 @@
 function execute_me(qr_link) {
     const pdf_version = this.document.getElementById("body");
 
-    // Set height of first page break according to the results div
-    const header_height = this.document.getElementById("report-header-div").offsetHeight;
-    const results_height = this.document.getElementById("results-div").offsetHeight;
-    this.document.getElementById("break-first-page").style.height = (1420 - header_height - results_height) + 'pt';
-
     // PDF styling
     var opt = {
         filename: 'myfile.pdf',
