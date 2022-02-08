99de3e2ebdb032537c2ea96a9896dac8772b3e99
Nicola Tarocco <ntarocco@gmail.com>
2022-02-08 10:24:11 +0100

    Auth service: pass debug option as CLI arg
    * closes #237

--- app-config/auth-service/auth_service/__main__.py
+++ app-config/auth-service/auth_service/__main__.py
@@ -1,9 +1,30 @@
-import tornado.ioloop
+import argparse
+
+from tornado.ioloop import IOLoop
 
 from . import make_app
 
 
+def configure_parser(parser) -> argparse.ArgumentParser:
+    parser.add_argument(
+        "--no-debug", help="Don't enable debug mode",
+        action="store_false",
+    )
+    parser.add_argument(
+        "--port",
+        help="The port to listen on",
+        default="8080"
+    )
+    return parser
+
+
+def main():
+    parser = configure_parser(argparse.ArgumentParser())
+    args = parser.parse_args()
+    app = make_app(debug=args.no_debug)
+    app.listen(args.port)
+    IOLoop.instance().start()
+
+
 if __name__ == "__main__":
-    app = make_app()
-    app.listen(8080)
-    tornado.ioloop.IOLoop.current().start()
+    main()
