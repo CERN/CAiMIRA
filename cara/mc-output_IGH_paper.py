a6b3d94f58dbb62cfce971ccf38fdc2bb2b8a07f
Andrejh <andre.henriques@cern.ch>
2022-02-16 22:22:26 +0100

    update of fig order

--- cara/mc-output_IGH_paper.py
+++ cara/mc-output_IGH_paper.py
@@ -1,29 +1,18 @@
 from cara.montecarlo import *
 from cara.model_scenarios_IGH_paper import *
 
-##Fig 1a
-#compare_concentration_curves_virus_IGH_paper([classroom_model_IGH_no_mask_windows_closed[0],classroom_model_IGH_no_mask_windows_open_breaks[0],
+#Fig 1c
+# compare_concentration_curves_virus_IGH_paper([classroom_model_IGH_no_mask_windows_closed[0],classroom_model_IGH_no_mask_windows_open_breaks[0],
 #                              classroom_model_IGH_no_mask_windows_open_alltimes[0]],
 #                             labels=['Windows closed', 'Window open during breaks', 'Window open at all times'],
 #                             colors=['tomato', 'lightskyblue', 'limegreen', '#1f77b4', 'seagreen', 'lightskyblue', 'deepskyblue'],
 #                             title='No mask - Spring/Summer period'
 #                             )
-#print_qd_info(classroom_model_IGH_no_mask_windows_closed[0])
-#print_qd_info(classroom_model_IGH_no_mask_windows_open_breaks[0])
-#print_qd_info(classroom_model_IGH_no_mask_windows_open_alltimes[0])
-#
-##Fig 1b
-#compare_concentration_curves_virus_IGH_paper([classroom_model_IGH_no_mask_windows_closed[0],classroom_model_IGH_no_mask_2windows_open_breaks[0],
-#                              classroom_model_IGH_no_mask_2windows_open_alltimes[0]],
-#                             labels=['Windows closed', '2 windows open during breaks', '2 windows open at all times'],
-#                             colors=['tomato', 'lightskyblue', 'limegreen', '#1f77b4', 'seagreen', 'lightskyblue', 'deepskyblue'],
-#                             title='No mask - Spring/Summer period'
-#                             )
-#print_qd_info(classroom_model_IGH_no_mask_windows_closed[0])
-#print_qd_info(classroom_model_IGH_no_mask_2windows_open_breaks[0])
-#print_qd_info(classroom_model_IGH_no_mask_2windows_open_alltimes[0])
+# print_qd_info(classroom_model_IGH_no_mask_windows_closed[0])
+# print_qd_info(classroom_model_IGH_no_mask_windows_open_breaks[0])
+# print_qd_info(classroom_model_IGH_no_mask_windows_open_alltimes[0])
 
-##Fig 1c
+##Fig 1d
 #compare_concentration_curves_virus_IGH_paper([classroom_model_IGH_no_mask_windows_closed[0],classroom_model_IGH_no_mask_windows_open_breaks_winter[0],
 #                              classroom_model_IGH_no_mask_windows_open_alltimes_winter[0], classroom_model_IGH_no_mask_windows_fully_open_alltimes_winter[0]],
 #                             labels=['Windows closed', 'Window fully open during breaks', 'Window slightly open at all times', 'Window fully open at all times'],
@@ -34,9 +23,19 @@ from cara.model_scenarios_IGH_paper import *
 #print_qd_info(classroom_model_IGH_no_mask_windows_open_breaks_winter[0])
 #print_qd_info(classroom_model_IGH_no_mask_windows_open_alltimes_winter[0])
 #print_qd_info(classroom_model_IGH_no_mask_windows_fully_open_alltimes_winter[0])
-#
 
-##Fig 1d
+##Fig 1e
+#compare_concentration_curves_virus_IGH_paper([classroom_model_IGH_no_mask_windows_closed[0],classroom_model_IGH_no_mask_2windows_open_breaks[0],
+#                              classroom_model_IGH_no_mask_2windows_open_alltimes[0]],
+#                             labels=['Windows closed', '2 windows open during breaks', '2 windows open at all times'],
+#                             colors=['tomato', 'lightskyblue', 'limegreen', '#1f77b4', 'seagreen', 'lightskyblue', 'deepskyblue'],
+#                             title='No mask - Spring/Summer period'
+#                             )
+#print_qd_info(classroom_model_IGH_no_mask_windows_closed[0])
+#print_qd_info(classroom_model_IGH_no_mask_2windows_open_breaks[0])
+#print_qd_info(classroom_model_IGH_no_mask_2windows_open_alltimes[0])
+
+##Fig 1f
 #compare_concentration_curves_virus_IGH_paper([classroom_model_IGH_no_mask_windows_closed[0],classroom_model_IGH_no_mask_2windows_open_breaks_winter[0],
 #                              classroom_model_IGH_no_mask_2windows_open_alltimes_winter[0], classroom_model_IGH_no_mask_2windows_fully_open_alltimes_winter[0]],
 #                             labels=['Windows closed', '2 windows fully open during breaks', '2 windows slighty open at all times', '2 windows fully open at all times'],
@@ -47,8 +46,8 @@ from cara.model_scenarios_IGH_paper import *
 #print_qd_info(classroom_model_IGH_no_mask_2windows_open_breaks_winter[0])
 #print_qd_info(classroom_model_IGH_no_mask_2windows_open_alltimes_winter[0])
 #print_qd_info(classroom_model_IGH_no_mask_2windows_fully_open_alltimes_winter[0])
-#
-##Fig 1e
+
+##Fig 1g
 #compare_concentration_curves_virus_IGH_paper([classroom_model_IGH_no_mask_windows_closed[0],classroom_model_IGH_no_mask_6windows_open_breaks[0],
 #                              classroom_model_IGH_no_mask_6windows_open_alltimes[0]],
 #                             labels=['Windows closed', '6 windows open during breaks', '6 windows open at all times'],
@@ -59,7 +58,7 @@ from cara.model_scenarios_IGH_paper import *
 #print_qd_info(classroom_model_IGH_no_mask_6windows_open_breaks[0])
 #print_qd_info(classroom_model_IGH_no_mask_6windows_open_alltimes[0])
 #
-##Fig 1f
+##Fig 1h
 #compare_concentration_curves_virus_IGH_paper([classroom_model_IGH_no_mask_windows_closed[0],classroom_model_IGH_no_mask_6windows_open_breaks_winter[0],
 #                              classroom_model_IGH_no_mask_6windows_open_alltimes_winter[0], classroom_model_IGH_no_mask_6windows_fully_open_alltimes_winter[0]],
 #                             labels=['Windows closed', '6 windows fully open during breaks', '6 windows slighty open at all times', '6 windows fully open at all times'],
@@ -70,7 +69,35 @@ from cara.model_scenarios_IGH_paper import *
 #print_qd_info(classroom_model_IGH_no_mask_6windows_open_breaks_winter[0])
 #print_qd_info(classroom_model_IGH_no_mask_6windows_open_alltimes_winter[0])
 #rint_qd_info(classroom_model_IGH_no_mask_6windows_fully_open_alltimes_winter[0])
+
+##Fig 1i
+#compare_concentration_curves_virus_IGH_paper([classroom_model_IGH_no_mask_windows_closed[0],
+#                             classroom_model_IGH_no_mask_6windows_open_breaks_endOfClass[0]],
+#                             labels=['Windows closed',
+#                                     '6 windows open during breaks + end of classes'],
+#                             colors=['tomato', '#1f77b4'],
+#                             title='Without mask - Spring/Summer period'
+#                             )
 #
+#print_qd_info(classroom_model_IGH_no_mask_windows_closed[0])
+##print_qd_info(classroom_model_IGH_no_mask_6windows_open_alltimes[0])
+#print_qd_info(classroom_model_IGH_no_mask_6windows_open_breaks_endOfClass[0])
+
+##Fig 1j
+#compare_concentration_curves_virus_IGH_paper([classroom_model_IGH_no_mask_windows_closed[0],
+#                             classroom_model_IGH_no_mask_6windows_open_breaks_endOfClass_winter[0]],
+#                             labels=['Windows closed',
+#                                     '6 windows open during breaks + end of classes'],
+#                             colors=['tomato', '#1f77b4'],
+#                             title='Without mask - Winter period'
+#                             )
+#
+#print_qd_info(classroom_model_IGH_no_mask_windows_closed[0])
+#print_qd_info(classroom_model_IGH_no_mask_6windows_open_alltimes_winter[0])
+#print_qd_info(classroom_model_IGH_no_mask_6windows_open_breaks_endOfClass_winter[0])
+
+
+
 ##Fig 2
 #compare_concentration_curves_virus_IGH_paper([classroom_model_IGH_no_mask_windows_closed[0],classroom_model_IGH_no_mask_windows_closed_1HEPA[0],
 #                              classroom_model_IGH_no_mask_windows_closed_2HEPA[0]],
@@ -130,29 +157,9 @@ from cara.model_scenarios_IGH_paper import *
 #print_qd_info(classroom_model_IGH_with_mask_2windows_open_alltimes_winter_2HEPA[0])
 #
 
-#compare_concentration_curves_virus_IGH_paper([classroom_model_IGH_no_mask_windows_closed[0],
-#                             classroom_model_IGH_no_mask_6windows_open_breaks_endOfClass[0]],
-#                             labels=['Windows closed',
-#                                     '6 windows open during breaks + end of classes'],
-#                             colors=['tomato', '#1f77b4'],
-#                             title='Without mask - Spring/Summer period'
-#                             )
-#
-#print_qd_info(classroom_model_IGH_no_mask_windows_closed[0])
-##print_qd_info(classroom_model_IGH_no_mask_6windows_open_alltimes[0])
-#print_qd_info(classroom_model_IGH_no_mask_6windows_open_breaks_endOfClass[0])
 
-#compare_concentration_curves_virus_IGH_paper([classroom_model_IGH_no_mask_windows_closed[0],
-#                             classroom_model_IGH_no_mask_6windows_open_breaks_endOfClass_winter[0]],
-#                             labels=['Windows closed',
-#                                     '6 windows open during breaks + end of classes'],
-#                             colors=['tomato', '#1f77b4'],
-#                             title='Without mask - Winter period'
-#                             )
-#
-#print_qd_info(classroom_model_IGH_no_mask_windows_closed[0])
-#print_qd_info(classroom_model_IGH_no_mask_6windows_open_alltimes_winter[0])
-#print_qd_info(classroom_model_IGH_no_mask_6windows_open_breaks_endOfClass_winter[0])
+
+
 
 #print_qd_info(classroom_model_IGH_no_mask_2windows_open_breaks_endOfClass_winter[0])
 #print_qd_info(classroom_model_IGH_no_mask_windows_open_breaks_endOfClass_winter[0])
@@ -163,16 +170,16 @@ from cara.model_scenarios_IGH_paper import *
 #print_qd_info(classroom_model_IGH_no_mask_2windows_open_breaks_endOfClass_winter[0])
 #print_qd_info(classroom_model_IGH_no_mask_windows_open_breaks_endOfClass_winter[0])
 
-compare_concentration_curves_virus_IGH_paper([classroom_model_IGH_no_mask_windows_closed[0],classroom_model_IGH_with_mask_windows_closed_1HEPA[0],
-                              classroom_model_IGH_with_mask_windows_closed_2HEPA[0]],
-                             labels=['No masks', '1 HEPA filter (2.5 ACH) + surgical masks', '2 HEPA filter (5 ACH) + surgical masks'],
-                             colors=['tomato', 'lightskyblue', 'limegreen', '#1f77b4', 'seagreen', 'lightskyblue', 'deepskyblue'],
-                             title='Combination of measures (windows closed) '
-                             )
-
-print_qd_info(classroom_model_IGH_no_mask_windows_closed[0])
-print_qd_info(classroom_model_IGH_with_mask_windows_closed_1HEPA[0])
-print_qd_info(classroom_model_IGH_with_mask_windows_closed_2HEPA[0])
+# compare_concentration_curves_virus_IGH_paper([classroom_model_IGH_no_mask_windows_closed[0],classroom_model_IGH_with_mask_windows_closed_1HEPA[0],
+#                               classroom_model_IGH_with_mask_windows_closed_2HEPA[0]],
+#                              labels=['No masks', '1 HEPA filter (2.5 ACH) + surgical masks', '2 HEPA filter (5 ACH) + surgical masks'],
+#                              colors=['tomato', 'lightskyblue', 'limegreen', '#1f77b4', 'seagreen', 'lightskyblue', 'deepskyblue'],
+#                              title='Combination of measures (windows closed) '
+#                              )
+#
+# print_qd_info(classroom_model_IGH_no_mask_windows_closed[0])
+# print_qd_info(classroom_model_IGH_with_mask_windows_closed_1HEPA[0])
+# print_qd_info(classroom_model_IGH_with_mask_windows_closed_2HEPA[0])
 
 
 #compare_concentration_curves([classroom_model_IGH_no_mask_windows_closed[1],classroom_model_IGH_no_mask_2windows_open_breaks[1],classroom_model_IGH_no_mask_2windows_open_breaks_endOfClass[1],
