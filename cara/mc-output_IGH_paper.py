37c5ff4ebadf5a101912f8bdc6032d5cb6455dfd
Andrejh <andre.henriques@cern.ch>
2022-02-17 10:56:06 +0100

    code labelling

--- cara/mc-output_IGH_paper.py
+++ cara/mc-output_IGH_paper.py
@@ -8,6 +8,7 @@ from cara.model_scenarios_IGH_paper import *
 #                             colors=['tomato', 'lightskyblue', 'limegreen', '#1f77b4', 'seagreen', 'lightskyblue', 'deepskyblue'],
 #                             title=''
 #                             )
+# print("\n############ Fig 1c ###########\n")
 # print_qd_info(classroom_model_IGH_no_mask_windows_closed[0])
 # print_qd_info(classroom_model_IGH_no_mask_windows_open_breaks[0])
 # print_qd_info(classroom_model_IGH_no_mask_windows_open_alltimes[0])
@@ -19,6 +20,7 @@ from cara.model_scenarios_IGH_paper import *
 #                             colors=['tomato', 'lightskyblue', '#1f77b4', 'limegreen', 'seagreen', 'lightskyblue', 'deepskyblue'],
 #                             title=''
 #                             )
+# print("\n############ Fig 1d ###########\n")
 # print_qd_info(classroom_model_IGH_no_mask_windows_closed[0])
 # print_qd_info(classroom_model_IGH_no_mask_windows_open_breaks_winter[0])
 # print_qd_info(classroom_model_IGH_no_mask_windows_open_alltimes_winter[0])
@@ -31,6 +33,7 @@ from cara.model_scenarios_IGH_paper import *
 #                             colors=['tomato', 'lightskyblue', 'limegreen', '#1f77b4', 'seagreen', 'lightskyblue', 'deepskyblue'],
 #                             title=''
 #                             )
+# print("\n############ Fig 1e ###########\n")
 # print_qd_info(classroom_model_IGH_no_mask_windows_closed[0])
 # print_qd_info(classroom_model_IGH_no_mask_2windows_open_breaks[0])
 # print_qd_info(classroom_model_IGH_no_mask_2windows_open_alltimes[0])
@@ -42,6 +45,7 @@ from cara.model_scenarios_IGH_paper import *
 #                             colors=['tomato', 'lightskyblue', '#1f77b4', 'limegreen', 'seagreen', 'lightskyblue', 'deepskyblue'],
 #                             title=''
 #                             )
+# print("\n############ Fig 1f ###########\n")
 # print_qd_info(classroom_model_IGH_no_mask_windows_closed[0])
 # print_qd_info(classroom_model_IGH_no_mask_2windows_open_breaks_winter[0])
 # print_qd_info(classroom_model_IGH_no_mask_2windows_open_alltimes_winter[0])
@@ -54,6 +58,7 @@ from cara.model_scenarios_IGH_paper import *
 #                             colors=['tomato', 'lightskyblue', 'limegreen', '#1f77b4', 'seagreen', 'lightskyblue', 'deepskyblue'],
 #                             title=''
 #                             )
+# print("\n############ Fig 1g ###########\n")
 # print_qd_info(classroom_model_IGH_no_mask_windows_closed[0])
 # print_qd_info(classroom_model_IGH_no_mask_6windows_open_breaks[0])
 # print_qd_info(classroom_model_IGH_no_mask_6windows_open_alltimes[0])
@@ -65,10 +70,11 @@ from cara.model_scenarios_IGH_paper import *
 #                             colors=['tomato', 'lightskyblue', '#1f77b4', 'limegreen', 'seagreen', 'lightskyblue', 'deepskyblue'],
 #                             title=''
 #                             )
+# print("\n############ Fig 1h ###########\n")
 # print_qd_info(classroom_model_IGH_no_mask_windows_closed[0])
 # print_qd_info(classroom_model_IGH_no_mask_6windows_open_breaks_winter[0])
 # print_qd_info(classroom_model_IGH_no_mask_6windows_open_alltimes_winter[0])
-# rint_qd_info(classroom_model_IGH_no_mask_6windows_fully_open_alltimes_winter[0])
+# print_qd_info(classroom_model_IGH_no_mask_6windows_fully_open_alltimes_winter[0])
 #
 # #Fig 1i
 # compare_concentration_curves_virus_IGH_paper([classroom_model_IGH_no_mask_windows_closed[0],
@@ -78,9 +84,8 @@ from cara.model_scenarios_IGH_paper import *
 #                             colors=['tomato', '#1f77b4'],
 #                             title=''
 #                             )
-#
+# print("\n############ Fig 1i ###########\n")
 # print_qd_info(classroom_model_IGH_no_mask_windows_closed[0])
-# #print_qd_info(classroom_model_IGH_no_mask_6windows_open_alltimes[0])
 # print_qd_info(classroom_model_IGH_no_mask_6windows_open_breaks_endOfClass[0])
 #
 # #Fig 1j
@@ -91,7 +96,7 @@ from cara.model_scenarios_IGH_paper import *
 #                             colors=['tomato', '#1f77b4'],
 #                             title=''
 #                             )
-#
+# print("\n############ Fig 1j ###########\n")
 # print_qd_info(classroom_model_IGH_no_mask_windows_closed[0])
 # print_qd_info(classroom_model_IGH_no_mask_6windows_open_alltimes_winter[0])
 # print_qd_info(classroom_model_IGH_no_mask_6windows_open_breaks_endOfClass_winter[0])
@@ -105,6 +110,7 @@ from cara.model_scenarios_IGH_paper import *
 #                             colors=['tomato', 'lightskyblue', 'limegreen', '#1f77b4', 'seagreen', 'lightskyblue', 'deepskyblue'],
 #                             title=''
 #                             )
+# print("\n############ Fig 2a ###########\n")
 # print_qd_info(classroom_model_IGH_no_mask_windows_closed[0])
 # print_qd_info(classroom_model_IGH_no_mask_windows_closed_1HEPA[0])
 # print_qd_info(classroom_model_IGH_no_mask_windows_closed_2HEPA[0])
@@ -115,44 +121,47 @@ from cara.model_scenarios_IGH_paper import *
 #                             colors=['tomato', 'lightskyblue', 'limegreen', '#1f77b4', 'seagreen', 'lightskyblue', 'deepskyblue'],
 #                             title=''
 #                             )
+# print("\n############ Fig 2b ###########\n")
 # print_qd_info(classroom_model_IGH_no_mask_windows_closed[0])
 # print_qd_info(classroom_model_IGH_with_mask_windows_closed[0])
 #
 #
 # #Fig 3b
-# compare_concentration_curves_virus_IGH_paper([classroom_model_IGH_no_mask_windows_closed[0],classroom_model_IGH_no_mask_2windows_open_alltimes_winter_bis[0],
+# compare_concentration_curves_virus_IGH_paper([classroom_model_IGH_no_mask_windows_closed_winter[0],classroom_model_IGH_no_mask_2windows_open_alltimes_winter_bis[0],
 #                              classroom_model_IGH_with_mask_2windows_open_alltimes_winter[0]],
 #                             labels=['Windows closed', '2 windows open at all times', '2 windows open at all times + surgical masks'],
 #                             colors=['tomato', 'lightskyblue', 'limegreen', '#1f77b4', 'seagreen', 'lightskyblue', 'deepskyblue'],
 #                             title=''
 #                             )
+# print("\n############ Fig 3b ###########\n")
 # print_qd_info(classroom_model_IGH_no_mask_windows_closed[0])
 # print_qd_info(classroom_model_IGH_no_mask_2windows_open_alltimes_winter_bis[0])
 # print_qd_info(classroom_model_IGH_with_mask_2windows_open_alltimes_winter[0])
 #
 # #Fig 3c
-# compare_concentration_curves_virus_IGH_paper([classroom_model_IGH_no_mask_windows_closed[0],classroom_model_IGH_with_mask_windows_closed_1HEPA[0],
+# compare_concentration_curves_virus_IGH_paper([classroom_model_IGH_no_mask_windows_closed_winter[0],classroom_model_IGH_with_mask_windows_closed_1HEPA[0],
 #                               classroom_model_IGH_with_mask_windows_closed_2HEPA[0]],
 #                              labels=['No masks', '1 HEPA filter (2.5 ACH) + surgical masks', '2 HEPA filter (5 ACH) + surgical masks'],
 #                              colors=['tomato', 'lightskyblue', 'limegreen', '#1f77b4', 'seagreen', 'lightskyblue', 'deepskyblue'],
 #                              title=''
 #                              )
-#
+# print("\n############ Fig 3c ###########\n")
 # print_qd_info(classroom_model_IGH_no_mask_windows_closed[0])
 # print_qd_info(classroom_model_IGH_with_mask_windows_closed_1HEPA[0])
 # print_qd_info(classroom_model_IGH_with_mask_windows_closed_2HEPA[0])
 #
 # #Fig 3d
-# compare_concentration_curves_virus_IGH_paper([classroom_model_IGH_no_mask_windows_closed[0],classroom_model_IGH_with_mask_2windows_open_alltimes_winter_1HEPA[0],
+# compare_concentration_curves_virus_IGH_paper([classroom_model_IGH_no_mask_windows_closed_winter[0],classroom_model_IGH_with_mask_2windows_open_alltimes_winter_1HEPA[0],
 #                              classroom_model_IGH_with_mask_2windows_open_alltimes_winter_2HEPA[0]],
 #                             labels=['Windows closed', '2 windows open at all times + 1 HEPA filter (2.5 ACH) + surgical masks', '2 windows open at all times + 2 HEPA filter (5 ACH) + surgical masks'],
 #                             colors=['tomato', 'lightskyblue', 'limegreen', '#1f77b4', 'seagreen', 'lightskyblue', 'deepskyblue'],
 #                             title=''
 #                             )
+# print("\n############ Fig 3d ###########\n")
 # print_qd_info(classroom_model_IGH_no_mask_windows_closed[0])
 # print_qd_info(classroom_model_IGH_with_mask_2windows_open_alltimes_winter_1HEPA[0])
 # print_qd_info(classroom_model_IGH_with_mask_2windows_open_alltimes_winter_2HEPA[0])
-#
+
 
 
 
