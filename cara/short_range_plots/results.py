4fa3d3b959bf48523e925781226b473b9091f21a
Andrejh <andre.henriques@cern.ch>
2022-02-21 14:43:06 +0100

    add example with 1 SR interaction

--- cara/short_range_plots/results.py
+++ cara/short_range_plots/results.py
@@ -13,10 +13,30 @@ from dataclasses import dataclass
 from cara.monte_carlo.data import symptomatic_vl_frequencies
 
 # print('\n<<<<<<<<<<< Peak viral concentration without short range for baseline scenarios >>>>>>>>>>>')
-# concentration_curve(models = [exposure_module_without_short_range(activity='Seated', expiration='Breathing', mask='No mask')],
-#                             labels = ['Baseline'],
+# concentration_curve(models = [exposure_module_without_short_range(activity='Light activity', expiration={"Speaking": 1, "Breathing": 2}, mask='No mask')],
+#                             labels = ['Background (long-range) concentration'],
+#                             labelsDose = ['Dose (long-range)'],
 #                             colors = ['royalblue'],
-# )
+#                             linestyles = ['--'],
+#                             thickness = [2])
+
+# print('\n<<<<<<<<<<< Peak viral concentration with short range interactions for baseline scenarios >>>>>>>>>>>')
+# concentration_curve(models=[exposure_module_with_short_range(
+#                                     activity='Light activity',
+#                                     expiration={"Speaking": 1, "Breathing": 2},
+#                                     mask='No mask',
+#                                     sr_presence=[(10.5, 11.0)],
+#                                     sr_activities=['Breathing']),
+#                             exposure_module_without_short_range(
+#                                 activity='Light activity',
+#                                 expiration={"Speaking": 1, "Breathing": 2},
+#                                 mask='No mask',)
+#                             ],
+#                             labels = ['Concentration with short range interactions', 'Background (long-range) concentration'],
+#                             labelsDose = ['Dose (full)', 'Dose (long-range)'],
+#                             colors = ['salmon', 'royalblue'],
+#                             linestyles = ['-', '--'],
+#                             thickness = [2, 2])
 
 # print('\n<<<<<<<<<<< Peak viral concentration with short range interactions for baseline scenarios >>>>>>>>>>>')
 # concentration_curve(models=[exposure_module_with_short_range(
@@ -36,26 +56,27 @@ from cara.monte_carlo.data import symptomatic_vl_frequencies
 #                             linestyles = ['-', '--'],
 #                             thickness = [2, 2])
 
-print('\n<<<<<<<<<<< Dose vs SR exposure time >>>>>>>>>>>')
-#Always assume 1h for the short range interactions.
-#Always assume that in each model there is only ONE short range interaction.
-plot_vD_vs_exposure_time(exp_models = [
-                            baseline_model(
-                                activity='Light activity',
-                                expiration={"Speaking": 2, "Breathing": 1},
-                                mask='No mask',
-                                sr_presence=[(8.5, 9.5)],
-                                sr_activities=['Breathing']),
-                            baseline_model(
-                                activity='Light activity',
-                                expiration={"Speaking": 2, "Breathing": 1},
-                                mask='No mask',
-                                sr_presence=[(8.5, 9.5)],
-                                sr_activities=['Speaking'])],
-                         labels = ['Baseline model breathing', 'Baseline model speaking'],
-                         colors=['royalblue', 'darkviolet'],
-                         linestyles=['solid', 'solid'],
-                         points=20,
-                         time_in_minutes=True,
-                         normalize_y_axis=True)
+
+# print('\n<<<<<<<<<<< Dose vs SR exposure time >>>>>>>>>>>')
+# #Always assume 1h for the short range interactions.
+# #Always assume that in each model there is only ONE short range interaction.
+# plot_vD_vs_exposure_time(exp_models = [
+#                             baseline_model(
+#                                 activity='Light activity',
+#                                 expiration={"Speaking": 2, "Breathing": 1},
+#                                 mask='No mask',
+#                                 sr_presence=[(8.5, 9.5)],
+#                                 sr_activities=['Breathing']),
+#                             baseline_model(
+#                                 activity='Light activity',
+#                                 expiration={"Speaking": 2, "Breathing": 1},
+#                                 mask='No mask',
+#                                 sr_presence=[(8.5, 9.5)],
+#                                 sr_activities=['Speaking'])],
+#                          labels = ['Baseline model breathing', 'Baseline model speaking'],
+#                          colors=['royalblue', 'darkviolet'],
+#                          linestyles=['solid', 'solid'],
+#                          points=20,
+#                          time_in_minutes=True,
+#                          normalize_y_axis=True)
 
