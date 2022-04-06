4306da1d24b9d99c35d855b93575e5dbd4deeee4
Andre Henriques <andre.henriques@cern.ch>
2022-04-06 12:20:12 +0200

    add scenario to concentration plot

--- cara/poster_plots/results_poster.py
+++ cara/poster_plots/results_poster.py
@@ -10,16 +10,17 @@ from scripts_poster import *
 
 #### Generic P(I) vs vl plot
 #plot_pi_vs_viral_load(activity='Heavy exercise', expiration='Shouting', mask='No mask')
-plot_pi_vs_viral_load_box_plot(activity='Heavy exercise', expiration='Shouting', mask='No mask')
+# plot_pi_vs_viral_load_box_plot(activity='Heavy exercise', expiration='Shouting', mask='No mask')
 
 
-### Paper classroom scenarios concentration curves
-# compare_concentration_curves(models = [
-#                                       classroom_model_lunch_vent(mask='No mask', lunch_break=False, ventilation=False, hepa=False),
-#                                       classroom_model_lunch_vent(mask='Type I', lunch_break=True, ventilation=False, hepa=False),
-#                                       classroom_model_lunch_vent(mask='Type I', lunch_break=True, ventilation=True, hepa=False),
-#                                       classroom_model_lunch_vent(mask='Type I', lunch_break=True, ventilation=True, hepa=True),
-#                                       ],
-#                            labels = ['no measures', 'mask and break',
-#                                       'Type I Masks, ventilation, break', 'Type I Masks and ventilation, break, hepa'],
-#                            colors = ['salmon', 'royalblue', (0, 0, 0.54, 1), (0, 0., 0.54, 0.4)])
+## Paper classroom scenarios concentration curves
+compare_concentration_curves(models = [
+                                      classroom_model_lunch_vent(mask='No mask', lunch_break=False, ventilation=False, hepa=False),
+                                      classroom_model_lunch_vent(mask='No mask', lunch_break=True, ventilation=False, hepa=False),
+                                      classroom_model_lunch_vent(mask='Type I', lunch_break=True, ventilation=False, hepa=False),
+                                      classroom_model_lunch_vent(mask='Type I', lunch_break=True, ventilation=True, hepa=False),
+                                      classroom_model_lunch_vent(mask='Type I', lunch_break=True, ventilation=True, hepa=True),
+                                      ],
+                           labels = ['no measures', 'break', 'mask and break',
+                                      'Type I Masks, ventilation, break', 'Type I Masks and ventilation, break, hepa'],
+                           colors = ['salmon', (0, 0, 0.54, 1), 'royalblue', (0, 0, 0.54, 0.6), (0, 0., 0.54, 0.4)])
