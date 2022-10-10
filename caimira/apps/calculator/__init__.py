a239b4f1ba00afc3eee2de3f92ffb25691495fd6
Luis Aleixo <luis.aleixo@cern.ch>
2022-10-10 16:53:39 +0200

    Increased version number

--- caimira/apps/calculator/__init__.py
+++ caimira/apps/calculator/__init__.py
@@ -33,7 +33,7 @@ from .user import AuthenticatedUser, AnonymousUser
 # calculator version. If the calculator needs to make breaking changes (e.g. change
 # form attributes) then it can also increase its MAJOR version without needing to
 # increase the overall CAiMIRA version (found at ``caimira.__version__``).
-__version__ = "4.4"
+__version__ = "4.5"
 
 
 class BaseRequestHandler(RequestHandler):
