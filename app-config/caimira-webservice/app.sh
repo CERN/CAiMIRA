b791581fe85c6e1b51c3e6ef1487917bf4a10974
Luis Aleixo <luis.aleixo@cern.ch>
2023-02-23 10:46:03 +0100

    updated docker files for expert app

--- app-config/caimira-webservice/app.sh
+++ app-config/caimira-webservice/app.sh
@@ -26,6 +26,9 @@ if [[ "$APP_NAME" == "caimira-webservice" ]]; then
 elif [[ "$APP_NAME" == "caimira-voila" ]]; then
     echo "Starting the voila service"
     voila caimira/apps/expert/ --port=8080 --no-browser --base_url=/voila-server/ --tornado_settings 'allow_origin=*'
+elif [[ "$APP_NAME" == "caimira-CO2-voila" ]]; then
+    echo "Starting the voila service"
+    voila caimira/apps/simulator/ --port=8080 --no-browser --base_url=/CO2-voila-server/ --tornado_settings 'allow_origin=*'
 else
     echo "No APP_NAME specified"
     exit 1
