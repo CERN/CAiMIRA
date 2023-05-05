c24b7043e384444b949710c4949209713ae2f69c
Luis Aleixo <luis.aleixo@cern.ch>
2023-05-05 16:33:19 +0200

    applied matomo script code

--- caimira/apps/static/js/usage-tracking.js
+++ caimira/apps/static/js/usage-tracking.js
@@ -1,43 +1,16 @@
-if (document.location.hostname == "caimira-test.web.cern.ch") {
-  var _paq = _paq || [];
+if (document.location.hostname == "caimira.web.cern.ch") {
+  var _paq = window._paq = window._paq || [];
   if (typeof AuthUserDomain !== 'undefined') {
-      _paq.push(["setCustomVariable", 1, "AuthUserDomain", AuthUserDomain, "visit"]);
+    _paq.push(["setCustomVariable", 1, "AuthUserDomain", AuthUserDomain, "visit"]);
   }
-  _paq.push(["trackPageView"]);
-  _paq.push(["enableLinkTracking"]);
-  (function () {
-    var u = "//piwik.web.cern.ch/";
-    _paq.push(["setTrackerUrl", u + "piwik.php"]);
-    _paq.push(["setSiteId", "7615"]);
-    var d = document,
-      g = d.createElement("script"),
-      s = d.getElementsByTagName("script")[0];
-    g.type = "text/javascript";
-    g.async = true;
-    g.defer = true;
-    g.src = u + "piwik.js";
-    s.parentNode.insertBefore(g, s);
-  })();
-} else if (document.location.hostname == "caimira.web.cern.ch") {
-  var _paq = _paq || [];
-  if (typeof AuthUserDomain !== 'undefined') {
-      _paq.push(["setCustomVariable", 1, "AuthUserDomain", AuthUserDomain, "visit"]);
-  }
-  _paq.push(["setCustomVariable", 1, "AuthUserDomain", AuthUserDomain, "visit"]);
-  _paq.push(["trackPageView"]);
-  _paq.push(["enableLinkTracking"]);
-  (function () {
-    var u = "//piwik.web.cern.ch/";
-    _paq.push(["setTrackerUrl", u + "piwik.php"]);
-    _paq.push(["setSiteId", "7616"]);
-    var d = document,
-      g = d.createElement("script"),
-      s = d.getElementsByTagName("script")[0];
-    g.type = "text/javascript";
-    g.async = true;
-    g.defer = true;
-    g.src = u + "piwik.js";
-    s.parentNode.insertBefore(g, s);
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="https://webanalytics.web.cern.ch/";
+    _paq.push(['setTrackerUrl', u+'matomo.php']);
+    _paq.push(['setSiteId', '264']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
   })();
 } else {
   console.log(
