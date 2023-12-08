2abdd130cffd623fd5d13e8f474d9bbc43c425d1
Nicola Tarocco <ntarocco@gmail.com>
2023-12-08 13:56:55 +0100

    config: remove Ref: config parsing

---
+++ caimira/enums.py
@@ -0,0 +1,13 @@
+from enum import Enum
+
+class ViralLoads(Enum):
+    COVID_OVERALL = "COVID overall"
+    SYMPTOMATIC_FREQUENCIES = "Symptomatic frequencies"
+
+
+class InfectiousDoses(Enum):
+    DISTRIBUTION  = "Distribution"
+
+
+class ViableToRNARatios(Enum):
+    DISTRIBUTION  = "Distribution"
