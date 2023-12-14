10fb5fb39b0643e555a0c38bf023e5b22c9d6643
Luis Aleixo <luis.aleixo@cern.ch>
2023-12-14 15:44:39 +0100

    updated enum values and removed fixme references

--- caimira/enums.py
+++ caimira/enums.py
@@ -1,13 +1,13 @@
 from enum import Enum
 
 class ViralLoads(Enum):
-    COVID_OVERALL = "COVID overall"
-    SYMPTOMATIC_FREQUENCIES = "Symptomatic frequencies"
+    COVID_OVERALL = "Ref: Viral load - covid_overal_vl_data"
+    SYMPTOMATIC_FREQUENCIES = "Ref: Viral load - symptomatic_vl_frequencies"
 
 
 class InfectiousDoses(Enum):
-    DISTRIBUTION  = "Distribution"
+    DISTRIBUTION  = "Ref: Infectious dose - infectious_dose_distribution"
 
 
 class ViableToRNARatios(Enum):
-    DISTRIBUTION  = "Distribution"
+    DISTRIBUTION  = "Ref: Viable to RNA ratio - viable_to_RNA_ratio_distribution"
