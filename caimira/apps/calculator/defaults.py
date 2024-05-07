0352675878844df97211f11faa14b8f84045aaf2
Luis Aleixo <luis.aleixo@cern.ch>
2024-05-07 15:16:55 +0200

    added input to model generator from form

--- caimira/apps/calculator/defaults.py
+++ caimira/apps/calculator/defaults.py
@@ -73,6 +73,7 @@ DEFAULTS = {
     'sensor_in_use': '',
     'short_range_option': 'short_range_no',
     'short_range_interactions': '[]',
+    'short_range_total_people': 1,
 }
 
 # ------------------ Activities ----------------------
