ecdbb1c2b39af2fcccc811c8de500d252391d396
lrdossan <luis.aleixo@cern.ch>
2024-06-24 17:47:07 +0200

    added redirect

--- app-config/caimira-public-docker-image/run_caimira.sh
+++ app-config/caimira-public-docker-image/run_caimira.sh
@@ -5,5 +5,6 @@ echo 'Please see https://gitlab.cern.ch/caimira/caimira for terms of use.'
 # Run a proxy for the apps (listening on 8080).
 nginx -c /opt/caimira/nginx.conf
 
+cd /opt/caimira/src/caimira
 # Run the calculator in the foreground.
 /opt/caimira/app/bin/python -m caimira.apps.calculator --port 8081 --no-debug
