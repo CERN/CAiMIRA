8b3af4be41f76e5e3652dc003baf8b4c3a5e6973
Nicola Tarocco <ntarocco@gmail.com>
2024-07-15 17:04:52 +0200

    wip: packages restructuring

--- caimira/calculator/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# This module is part of CAiMIRA. Please see the repository at
-# https://gitlab.cern.ch/caimira/caimira for details of the license and terms of use.
-"""
-Documentation for the CAiMIRA package
-
-"""
-
-__version__ = "1.0.0"
