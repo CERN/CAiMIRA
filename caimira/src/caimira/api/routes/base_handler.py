7ce041b23bf0484466929ce36a4ee9aabc45f00c
Matteo Manzinello <matteo@manzinello.com>
2025-01-15 11:59:34 +0100

    def options in base_handler

--- caimira/src/caimira/api/routes/base_handler.py
+++ caimira/src/caimira/api/routes/base_handler.py
@@ -1,13 +1,15 @@
 import tornado.web
 
-
 class BaseRequestHandler(tornado.web.RequestHandler):
     def set_default_headers(self):
         self.set_header("Access-Control-Allow-Origin", "*")
-        self.set_header("Access-Control-Allow-Headers", "x-requested-with")
+        self.set_header("Access-Control-Allow-Headers", "x-requested-with, Content-Type")
         self.set_header("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
+
+    def options(self):
+        self.set_status(204)
+        self.finish()
 
     def write_error(self, status_code, **kwargs):
         self.set_status(status_code)
         self.write({"message": kwargs.get('exc_info')[1].__str__()})
-        
