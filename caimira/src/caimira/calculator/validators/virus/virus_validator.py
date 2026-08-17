cc8c4debad193fee6787ccea62c59711a4734274
mheie <mari.eldegard.heie@cern.ch>
2026-08-17 13:24:03 +0200

    fix: use same infected instance for long and short range

--- caimira/src/caimira/calculator/validators/virus/virus_validator.py
+++ caimira/src/caimira/calculator/validators/virus/virus_validator.py
@@ -251,10 +251,12 @@ class VirusFormData(FormData):
 
         return models.Room(volume=volume, inside_temp=models.PiecewiseConstant((0, 24), (inside_temp,)), humidity=humidity) # type: ignore
 
-    def build_mc_model(self) -> mc.ExposureModelGroup:
+    def build_mc_model(self, sample_size=None) -> mc.ExposureModelGroup:
+        sample_size = sample_size or self.data_registry.monte_carlo['sample_size']
+        
         room: models.Room = self.initialize_room()
         ventilation: models._VentilationBase = self.ventilation()
-        infected_population: models.InfectedPopulation = self.infected_population()
+        infected_population: models.InfectedPopulation = self.infected_population().build_model(sample_size)
 
         short_range = defaultdict(list)
         if self.short_range_option == "short_range_yes":
@@ -326,7 +328,7 @@ class VirusFormData(FormData):
 
     def build_model(self, sample_size=None) -> models.ExposureModelGroup:
         sample_size = sample_size or self.data_registry.monte_carlo['sample_size']
-        return self.build_mc_model().build_model(sample_size)
+        return self.build_mc_model(sample_size).build_model(sample_size)
 
     def build_CO2_model(self, sample_size=None) -> models.CO2ConcentrationModel:
         """
